4b631f80535bf8a8c4c3b3a9f19d2da73735a501
roscoevanderboom <roscoevanderboom@protonmail.com>
2020-05-02 19:13:59 +0800

    update landing page

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -9,17 +9,13 @@ function Landing() {
             <div className="card-body">
                 <h3>Welcome new user</h3>
                 <p>You currently don't have an account. Click below to create and
-                anonymous account. You can also watch a demo video first to
-                see how the game is played.
+                anonymous account.
                 </p>
             </div>
             <div className="card-footer d-flex justify-content-around">
                 <button className="btn btn-outline-info"
                     onClick={signInAnonymously}>
                     Create account
-                </button>
-                <button className="btn btn-outline-info">
-                    View demo video
                 </button>
             </div>
         </div>
