a4edbeaed6386657523f79eac342fff7273dba5a
roscoevanderboom <roscoevanderboom@protonmail.com>
2021-06-24 17:10:04 +0800

    minor update

--- src/firebase/api/index.js
+++ src/firebase/api/index.js
@@ -1,9 +1,3 @@
 export let credentials = {
-  apiKey: "AIzaSyD50CvuhC1IJVFhscE-UQXKeKzsFtB08n8",
-  authDomain: "tic-tac-farm.web.app",
-  databaseURL: "https://tic-tac-farm.firebaseio.com",
-  projectId: "tic-tac-farm",
-  storageBucket: "tic-tac-farm.appspot.com",
-  messagingSenderId: "685472119585",
-  appId: "1:685472119585:web:8aa7b23d29f95127",
+  
 };
